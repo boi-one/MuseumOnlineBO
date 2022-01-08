06afc7cd162fd2ab1ae05d41f7f0864ec6690031
unknown <30570@ma-web.nl>
2022-01-08 17:26:18 +0100

    slideshow

--- website/script.js
+++ website/script.js
@@ -1,1 +1,8 @@
+const myImage = document.getElementById('myImage');
 
+let paintings = ["img/tentoonstelling1.jpg", "img/tentoonstelling2.jpg", "img/tentoonstelling3.jpg", "img/tentoonstelling4.jpg"]
+
+function changeImage(index){
+    myImage.src = tentoonstelling[index];
+    
+}
